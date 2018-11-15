60043d96b7e11b8493594a35dd9c1250588808f0
sk <sk@typedivision.de>
2018-11-15 21:02:43 +0100

    add init script

---
+++ pacstage-init.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Initialize the pacstage build environment.
+CMDPATH=$(cd "$(dirname $0)" && pwd)
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Initialize the pacstage build environment."
+  echo "usage: ${0##*/}"
+  echo && exit 0
+fi
+
+if ! [ "$(command -v pacman)" ]; then
+  echo "==> ERROR: the pacstage build environment needs an archlinux base system to run"
+  exit 1
+fi
+
+echo "==> Install host tools"
+pacman -Sy --noconfirm --needed arch-install-scripts python3 wget ca-certificates tar gzip bzip2 xz unzip git bubblewrap tmux vim
+
+BUILD_DIR="$CMDPATH"/build
+if ! [ -d "$BUILD_DIR" ]; then
+  echo "==> Create build dir"
+  mkdir -p "$BUILD_DIR"/conf
+  echo 'BBLAYERS = "${TOPDIR}/../meta"' > "$BUILD_DIR"/conf/bblayers.conf
+fi
+
+echo ">"
+echo "> The pacstage build environment is ready for use now"
+if ! [ "$(command -v bitbake)" ]; then
+  echo "> You may need to set       PATH=$CMDPATH/bitbake/bin:\$PATH"
+fi
+echo "> Finally go to             $BUILD_DIR"
+echo "> And start building with   bitbake <recipe>"
+echo "> Get available recipes by  bitbake -s"
+echo ">"
